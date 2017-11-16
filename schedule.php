f45252d099374cd0bce74d17eb2729d7f068bef4
Tobyn Collinsworth <tobyn.collinsworth@gmail.com>
2017-11-16 16:59:10 -0500

    Modified the schedule page

--- schedule.php
+++ schedule.php
@@ -12,18 +12,24 @@ if (isset($_POST['service'])) {
 $services = get_services($db);
 ?>
     <main>
-        <p>Please select the option that best suits your needs:</p>
-        <form method="post" action="submit.php">
-            <select name="service">
-            <?php
-                for($i = 0; $i < count($services); $i++){
-                    echo "<option value='{$services[$i]['id']}'>{$services[$i]['description']}</option>";
-                }
-            ?>
-            </select>
-            <br>
-            <input type="submit" id="submit" value="submit">
-        </form>
+        <div class="flex-grid">
+            <div class="col">
+                <p>Please select the option that best suits your needs:</p>
+                <form method="post" action="submit.php">
+                    <select name="service">
+                        <?php
+                        for($i = 0; $i < count($services); $i++){
+                            echo "<option value='{$services[$i]['id']}'>{$services[$i]['description']}</option>";
+                        }
+                        ?>
+                    </select>
+                    <br>
+                    <label for="note">Notes:</label>
+                    <textarea name="note" cols="30" rows="5"></textarea>
+                    <input type="submit" id="submit" value="submit">
+                </form>
+            </div>
+        </div>
     </main>
 
 <?php include 'inc/footer.php'; ?>
