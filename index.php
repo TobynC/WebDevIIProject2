a38c44cb8ffd68f70d1fa25f408fa619a3348648
Tobyn Collinsworth <tobyn.collinsworth@gmail.com>
2017-11-16 15:01:48 -0500

    Refactoring

--- index.php
+++ index.php
@@ -1,35 +1,34 @@
 <?php
 
-   session_start();   
-   
+session_start();
 
-$description='Project 2, Collinsworth/Dodds';
-$pageTitle='Laptop Repair';
+
+$description = 'Project 2, Collinsworth/Dodds';
+$pageTitle = 'Laptop Repair';
 include "inc/header.php";
 echo '<main>';
 echo '<p>Welcome to Gabe and Tobyn\'s laptop repair. We offer the following services for your laptop:</p>';
 echo '<ul>'
-. '<li>'
-        . 'Software Troubleshooting and "Tune-Up"</li>'
-        . '<li>'
-        . 'Virus Detection and Removal</li>'
-        . '<li>'
-        . 'Battery Replacement</li>'
-        . '<li>'
-        . 'Screen Repair/Replacement</li>'
-        . '<li>'
-        . 'Hardware Troubleshooting</li>'
-        . '</ul>';
-if (isset($_SESSION['type'])){
-  
-   $login_status = $_SESSION['login'];
-   if ($login_status == 'accept_existing'||$login_status == 'accept_new') {
-          echo '<button onclick="location.href=\'login_files/logout.php\'">Log Out</button>';
-          }
+    . '<li>'
+    . 'Software Troubleshooting and "Tune-Up"</li>'
+    . '<li>'
+    . 'Virus Detection and Removal</li>'
+    . '<li>'
+    . 'Battery Replacement</li>'
+    . '<li>'
+    . 'Screen Repair/Replacement</li>'
+    . '<li>'
+    . 'Hardware Troubleshooting</li>'
+    . '</ul>';
+if (isset($_SESSION['type'])) {
+
+    $login_status = $_SESSION['login'];
+    if ($login_status == 'accept_existing' || $login_status == 'accept_new') {
+        echo '<button onclick="location.href=\'login_files/logout.php\'">Log Out</button>';
+    }
+} else {
+    echo '<button onclick="location.href=\'login_files/login_start.php\'">Login</button>';
 }
-else{
-            echo '<button onclick="location.href=\'login_files/login_start.php\'">Login</button>';
-          }
 
 echo '<button onclick="location.href=\'schedule.php\'">Schedule Appointment</button>';
 echo '<button onclick="location.href=\'past.php\'">Appointment History</button>';
--- schedule.php
+++ schedule.php
@@ -1,22 +1,24 @@
 <?php
-
 $description='Project 2, Collinsworth/Dodds';
 $pageTitle='Laptop Repair';
-if(isset($_POST['service'])){
-unset($_POST['service']);}
 include "inc/header.php";
+
+if (isset($_POST['service'])) {
+    unset($_POST['service']);
+}
+
 echo '<main>';
 echo '<p>Please select the option that best suits your needs:</p>';
 echo '<form method="post" action="submit.php"><select name="service">'
-. '<option value="1">Software Troubleshooting and "Tune-Up"</option>'
-        . '<option value="2">Virus Detection and removal</option>'
-        . '<option value="3">Battery Replacement</option>'
-        . '<option value="4">Screen Repair/Replacement</option>'
-        . '<option value="5">Hardware Troubleshooting</option></select><br>'
-        . '<input type="submit" id="submit" value="submit">'
-        . '</form>';
+    . '<option value="1">Software Troubleshooting and "Tune-Up"</option>'
+    . '<option value="2">Virus Detection and removal</option>'
+    . '<option value="3">Battery Replacement</option>'
+    . '<option value="4">Screen Repair/Replacement</option>'
+    . '<option value="5">Hardware Troubleshooting</option></select><br>'
+    . '<input type="submit" id="submit" value="submit">'
+    . '</form>';
 
 echo '<button onclick="location.href=\'index.php\'">Return to Home</button>';
-
 echo '</main>';
+
 include 'inc/footer.php';
